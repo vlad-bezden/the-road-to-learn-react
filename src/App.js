90587e1f7a9bce72dbb918ae862cfba88c6542dc
Vlad Bezden <vlad.bezden@gmail.com>
2018-03-10 15:48:25 -0500

    completed 'Unidirectional Data Flow' chapter

--- src/App.js
+++ src/App.js
@@ -20,10 +20,27 @@ const list = [
 ]
 
 class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            list: list,
+        }
+
+        this.onDismiss = this.onDismiss.bind(this)
+    }
+
+    onDismiss(id) {
+        const isNodId = item => item.objectID !== id
+        const updatedList = this.state.list.filter(isNodId)
+        this.setState({list: updatedList})
+    }
+
     render() {
         return (
             <div>
-                {list.map(item =>
+                {this.state.list.map(item =>
                     <div key={item.objectID}>
                         <span>
                             <a href={item.url}>{item.title}</a>
@@ -31,6 +48,14 @@ class App extends Component {
                         <span>{item.author}</span>
                         <span>{item.num_comments}</span>
                         <span>{item.points}</span>
+                        <span>
+                            <button
+                                onClick={() => this.onDismiss(item.objectID)}
+                                type='button'
+                            >
+                                Dismiss
+                            </button>
+                        </span>
                     </div>
                 )}
             </div>
