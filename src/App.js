c37520f7714ccfbe7c4b406b15dcf5cf522e7f70
Vlad Bezden <vlad.bezden@gmail.com>
2018-03-10 14:43:53 -0500

    Finished 'Introduction to React' chapter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
 
+const list = [
+    {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+]
+
 class App extends Component {
     render() {
-        const helloWorld = 'Hello World'
         return (
             <div>
-                <h2>{helloWorld}</h2>
+                {list.map(item =>
+                    <div key={item.objectID}>
+                        <span>
+                            <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+                )}
             </div>
         )
     }
