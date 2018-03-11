56bd64fdce6cee3be4343083a78a1ebf47ee5deb
Vlad Bezden <vlad.bezden@gmail.com>
2018-03-10 19:40:56 -0500

    finished  'Split Up Components' chapter

--- src/App.js
+++ src/App.js
@@ -50,13 +50,39 @@ class App extends Component {
         const { searchTerm, list } = this.state
         return (
             <div>
-                <form>
-                    <input
-                        type='text'
-                        value={searchTerm}
-                        onChange={this.onSearchChange} />
-                </form>
-                {list.filter(isSearched(searchTerm)).map(item =>
+                <Search
+                    value={searchTerm}
+                    onChange={this.onSearchChange} />
+                <Table
+                    list={list}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss} />
+            </div>
+        )
+    }
+}
+
+class Search extends Component {
+    render() {
+        const { value, onChange } = this.props
+        return (
+            <form>
+                <input
+                    type='text'
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        )
+    }
+}
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props
+        return (
+            <div>
+                {list.filter(isSearched(pattern)).map(item =>
                     <div key={item.objectID}>
                         <span>
                             <a href={item.url}>{item.title}</a>
@@ -66,10 +92,8 @@ class App extends Component {
                         <span>{item.points}</span>
                         <span>
                             <button
-                                onClick={() => this.onDismiss(item.objectID)}
-                                type='button'
-                            >
-                                Dismiss
+                                onClick={() => onDismiss(item.objectID)}
+                                type='button'>Dismiss
                             </button>
                         </span>
                     </div>
