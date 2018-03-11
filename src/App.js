d2ae7ce07bc62204a41804d674d27aa5256e2973
Vlad Bezden <vlad.bezden@gmail.com>
2018-03-11 13:20:32 -0400

    finished 'ES6 spread Operators' chapter

--- src/App.js
+++ src/App.js
@@ -55,9 +55,12 @@ class App extends Component {
     }
 
     onDismiss(id) {
-        const isNodId = item => item.objectID !== id
-        const updatedList = this.state.list.filter(isNodId)
-        this.setState({ list: updatedList })
+        const isNotId = item => item.objectID !== id
+        const updatedHits = this.state.result.hits.filter(isNotId)
+        this.setState({
+            result: { ...this.state.result, hits: updatedHits }
+        })
+
     }
 
     render() {
