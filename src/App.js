e03a19ff85bcf51b5332c9bea5bce481f0686d06
Vlad Bezden <vlad.bezden@gmail.com>
2018-03-11 11:52:19 -0400

    completed 'Fetching Data' chapter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
 
+const DEFAULT_QUERY = 'react'
 
-const list = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-]
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search'
+const PARAM_SEARCH = 'query='
 
 const largeColumn = {
     width: '40%',
@@ -42,12 +28,26 @@ class App extends Component {
         super(props)
 
         this.state = {
-            list: list,
-            searchTerm: '',
+            result: null,
+            searchTerm: DEFAULT_QUERY,
         }
 
+        this.setSearchTopStories = this.setSearchTopStories.bind(this)
         this.onSearchChange = this.onSearchChange.bind(this)
         this.onDismiss = this.onDismiss.bind(this)
+    }
+
+    setSearchTopStories(result) {
+        this.setState({ result })
+    }
+
+    componentDidMount() {
+        const { searchTerm } = this.state
+
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+            .then(response => response.json())
+            .then(result => this.setSearchTopStories(result))
+            .catch(error => error)
     }
 
     onSearchChange(event) {
@@ -61,7 +61,10 @@ class App extends Component {
     }
 
     render() {
-        const { searchTerm, list } = this.state
+        const { searchTerm, result } = this.state
+
+        if (!result) { return null }
+
         return (
             <div className='page'>
                 <div className='interactions'>
@@ -72,7 +75,7 @@ class App extends Component {
                 </Search>
                 </div>
                 <Table
-                    list={list}
+                    list={result.hits}
                     pattern={searchTerm}
                     onDismiss={this.onDismiss} />
             </div>
