be2207b97566f5d17eace63336897cd8c39033e7
Vlad Bezden <vlad.bezden@gmail.com>
2018-04-11 09:28:54 -0400

    changed code to use axios instead of fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 
 const DEFAULT_QUERY = 'react'
@@ -23,6 +24,7 @@ const smallColumn = {
 };
 
 class App extends Component {
+    _isMounted = false
 
     constructor(props) {
         super(props)
@@ -43,9 +45,15 @@ class App extends Component {
     }
 
     componentDidMount() {
+        this._isMounted = true
+
         const { searchTerm } = this.state
         this.setState({ searchKey: searchTerm })
         this.fetchSearchTopStories(searchTerm)
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false
     }
 
     setSearchTopStories(result) {
@@ -74,11 +82,10 @@ class App extends Component {
     }
 
     fetchSearchTopStories(searchTerm, page = 0) {
-        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}\
+        axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}\
         &${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-            .then(response => response.json())
-            .then(result => this.setSearchTopStories(result))
-            .catch(error => this.setState({ error }))
+            .then(result => this._isMounted && this.setSearchTopStories(result.data))
+            .catch(error => this._isMounted && this.setState({ error }))
     }
 
     onSearchChange(event) {
