b345aef1a3f0e10c4148158866766c445681cd37
Vlad Bezden <vlad.bezden@gmail.com>
2018-05-19 11:12:23 -0400

    Added jest and enzime tests, eslint, PropTypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import axios from 'axios'
 import './App.css'
+
 
 const DEFAULT_QUERY = 'react'
 const DEFAULT_HPP = '100'
@@ -12,227 +14,259 @@ const PARAM_PAGE = 'page='
 const PARAM_HPP = 'hitsPerPage='
 
 const largeColumn = {
-    width: '40%',
+  width: '40%',
 }
 
 const midColumn = {
-    width: '30%',
+  width: '30%',
 }
 
 const smallColumn = {
-    width: '10%',
+  width: '10%',
 }
 
 class App extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            results: null,
-            searchKey: '',
-            searchTerm: DEFAULT_QUERY,
-            error: null,
-            isLoading: false,
-        }
-
-        this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
-        this.setSearchTopStories = this.setSearchTopStories.bind(this)
-        this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
-        this.onSearchChange = this.onSearchChange.bind(this)
-        this.onSearchSubmit = this.onSearchSubmit.bind(this)
-        this.onDismiss = this.onDismiss.bind(this)
+    this.state = {
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null,
+      isLoading: false,
     }
 
-    componentDidMount() {
-        this._isMounted = true
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
+  }
 
-        const { searchTerm } = this.state
-        this.setState({ searchKey: searchTerm })
-        this.fetchSearchTopStories(searchTerm)
+  componentDidMount() {
+    this.isMounted = true
+
+    const { searchTerm } = this.state
+    this.setState({ searchKey: searchTerm })
+    this.fetchSearchTopStories(searchTerm)
+  }
+
+  componentWillUnmount() {
+    this.isMounted = false
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state
+    this.setState({ searchKey: searchTerm })
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm)
     }
+    event.preventDefault()
+  }
 
-    componentWillUnmount() {
-        this._isMounted = false
-    }
+  onDismiss(id) {
+    const { searchKey, results } = this.state
+    const { hits, page } = results[searchKey]
 
-    setSearchTopStories(result) {
-        const { hits, page } = result
-        const { searchKey, results } = this.state
+    const isNotId = item => item.objectID !== id
+    const updatedHits = hits.filter(isNotId)
 
-        const oldHits = results && results[searchKey]
-            ? results[searchKey].hits
-            : []
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page },
+      },
+    })
+  }
 
-        const updatedHits = [
-            ...oldHits,
-            ...hits
-        ]
+  setSearchTopStories(result) {
+    const { hits, page } = result
+    const { searchKey, results } = this.state
 
-        this.setState({
-            results: {
-                ...results,
-                [searchKey]: { hits: updatedHits, page }
-            },
-            isLoading: false
-        })
-    }
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : []
 
-    needsToSearchTopStories(searchTerm) {
-        return !this.state.results[searchTerm]
-    }
+    const updatedHits = [
+      ...oldHits,
+      ...hits,
+    ]
 
-    fetchSearchTopStories(searchTerm, page = 0) {
-        this.setState({ isLoading: true })
-        axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}\
-        &${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-            .then(result => this._isMounted && this.setSearchTopStories(result.data))
-            .catch(error => this._isMounted && this.setState({ error }))
-    }
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page },
+      },
+      isLoading: false,
+    })
+  }
 
-    onSearchChange(event) {
-        this.setState({ searchTerm: event.target.value })
-    }
+  fonSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
 
-    onSearchSubmit(event) {
-        const { searchTerm } = this.state
-        this.setState({ searchKey: searchTerm })
-        if (this.needsToSearchTopStories(searchTerm)) {
-            this.fetchSearchTopStories(searchTerm)
-        }
-        event.preventDefault()
-    }
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm]
+  }
 
-    onDismiss(id) {
-        const { searchKey, results } = this.state
-        const { hits, page } = results[searchKey]
+  fetchSearchTopStories(searchTerm, page = 0) {
+    this.setState({ isLoading: true })
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}\
+    &${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this.isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this.isMounted && this.setState({ error }))
+  }
 
-        const isNotId = item => item.objectID !== id
-        const updatedHits = hits.filter(isNotId)
+  render() {
+    const {
+      searchTerm,
+      results,
+      searchKey,
+      error,
+      isLoading,
+    } = this.state
 
-        this.setState({
-            results: {
-                ...results,
-                [searchKey]: { hits: updatedHits, page }
-            }
-        })
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0
 
-    }
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || []
 
-    render() {
-        const {
-            searchTerm,
-            results,
-            searchKey,
-            error,
-            isLoading,
-        } = this.state
-
-        const page = (
-            results &&
-            results[searchKey] &&
-            results[searchKey].page
-        ) || 0
-
-        const list = (
-            results &&
-            results[searchKey] &&
-            results[searchKey].hits
-        ) || []
-
-        return (
-            <div className='page'>
-                <div className='interactions'>
-                    <Search
-                        value={searchTerm}
-                        onChange={this.onSearchChange}
-                        onSubmit={this.onSearchSubmit}>
-                        Search
-                    </Search>
-                </div>
-                {error
-                    ? <div className='interactions'>
-                        <p>Something went wrong</p>
-                    </div>
-                    : <Table
-                        list={list}
-                        onDismiss={this.onDismiss}
-                    />
-                }
-                <div className='interactions'>
-                    {isLoading
-                        ? <Loading />
-                        : <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
-                            More
-                       </Button>
-                    }
-                </div>
-            </div>
-        )
-    }
-}
-
-const Search = ({ value, onChange, onSubmit, children }) => {
-    // do something
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                type='text'
-                value={value}
-                onChange={onChange}
+      <div className='page'>
+        <div className='interactions'>
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        {
+          error ?
+            <div className='interactions'>
+              <p>Something went wrong</p>
+            </div>
+            : <Table
+              list={list}
+              onDismiss={this.onDismiss}
             />
-            <button type='submit'>
-                {children}
-            </button>
-        </form>
+        }
+        <div className='interactions'>
+          {isLoading ?
+            <Loading />
+            :
+            <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+              More
+            </Button>
+          }
+        </div>
+      </div>
     )
+  }
 }
 
-const Table = ({ list, onDismiss }) =>
-    <div className='table'>
-        {list.map(item =>
-            <div key={item.objectID} className='table-row'>
-                <span style={largeColumn}>
-                    <a href={item.url}>{item.title}</a>
-                </span>
-                <span style={midColumn}>
-                    {item.author}
-                </span>
-                <span style={smallColumn}>
-                    {item.num_comments}
-                </span>
-                <span style={smallColumn}>
-                    {item.points}
-                </span>
-                <span style={smallColumn}>
-                    <Button
-                        onClick={() => onDismiss(item.objectID)}
-                        className='button-inline'
-                    >
-                        Dismiss
-                    </Button>
-                </span>
-            </div>
-        )}
-    </div>
-
-const Button = ({ onClick, className = '', children }) =>
-    <button
-        onClick={onClick}
-        className={className}
-        type='button'
-    >
+const Search = ({
+  value, onChange, onSubmit, children,
+}) =>
+  // do something
+  (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        value={value}
+        onChange={onChange}
+      />
+      <button type='submit'>
         {children}
-    </button>
+      </button>
+    </form>
+  )
 
+
+const Table = ({ list, onDismiss }) => (
+  <div className='table'>
+    {list.map(item => (
+      <div key={item.objectID} className='table-row'>
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>
+          {item.author}
+        </span>
+        <span style={smallColumn}>
+          {item.num_comments}
+        </span>
+        <span style={smallColumn}>
+          {item.points}
+        </span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className='button-inline'
+          >
+            Dismiss
+          </Button>
+        </span>
+      </div>
+    ))}
+  </div>
+)
+
+const Button = ({ onClick, className, children }) => (
+  <button
+    onClick={onClick}
+    className={className}
+    type='button'
+  >
+    {children}
+  </button>
+)
 
 const Loading = () =>
-    <div>Loading ...</div>
+  <div>Loading ...</div>
 
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+}
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    objectID: PropTypes.string.isRequired,
+    author: PropTypes.string,
+    url: PropTypes.string,
+    num_comments: PropTypes.number,
+    points: PropTypes.number,
+  })).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+}
+
+Button.defaultProps = {
+  className: '',
+}
 
 export default App
 
 export {
-    Button,
-    Search,
-    Table,
+  Button,
+  Search,
+  Table,
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,74 +1,80 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import App, { Search, Button, Table } from './App'
 
-describe('App', () => {
-    it('renderes without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(<App />, div)
-        ReactDOM.unmountComponentAtNode(div)
-    })
+Enzyme.configure({ adapter: new Adapter() })
 
-    test('has a valid snapshot', () => {
-        const component = renderer.create(
-            <App />
-        )
-        let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+describe('App', () => {
+  it('renderes without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<App />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
 
 describe('Search', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(<Search>Search</Search>, div);
-        ReactDOM.unmountComponentAtNode(div)
-    })
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Search>Search</Search>, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 
-    test('has a valid snapshot', () => {
-        const component = renderer.create(
-            <Search>Search</Search>
-        )
-        let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Search>Search</Search>)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
 
 describe('Button', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(<Button>Give Me More</Button>, div)
-        ReactDOM.unmountComponentAtNode(div)
-    })
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Button>Give Me More</Button>, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
 
-    test('has a valid snapshot', () => {
-        const component = renderer.create(
-            <Button>Give Me More</Button>
-        )
-        let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Button>Give Me More</Button>)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
 
 describe('Table', () => {
-    const props = {
-        list: [
-            { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
-            { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
-        ]
-    }
+  const props = {
+    list: [
+      {
+        title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y',
+      },
+      {
+        title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z',
+      },
+    ],
+  }
 
-    it('renders without crashing', () => {
-        const div = document.createElement('div')
-        ReactDOM.render(<Table {...props} />, div)
-    })
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Table {...props} />, div)
+  })
 
-    test('has a valid snapshot', () => {
-        const component = renderer.create(
-            <Table {...props} />
-        )
-        let tree = component.toJSON()
-        expect(tree).toMatchSnapshot()
-    })
+  it('shows tow items in list', () => {
+    const element = shallow(<Table {...props} />)
+
+    expect(element.find('.table-row').length).toBe(2)
+  })
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(<Table {...props} />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
